1ba0d77606337a22931b7e49ba77e6aabf594a9c
Shadrach247 <soshadrach@gmail.com>
2022-08-19 12:53:05 +0100

    update dev

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -1,3 +1,5 @@
+const sha256 = require('sha256');
+
 function Blockchain() {
     this.chain = [];
     this.pendingTransactions = [];
@@ -33,8 +35,10 @@ Blockchain.prototype.createNewTransaction = function(amount, sender, recipient) 
     return this.getLastBlock().index + 1;
 };
 
-Blockchain.prototype.hashBlock = function(blockData) {
-
+Blockchain.prototype.hashBlock = function(previousBlockHash, currentBlockData, nonce) {
+    const dataAsString = previousBlockHash + nonce.toString() + JSON.stringify(currentBlockData);
+    const hash = sha256(dataAsString);
+    return hash;
 };
 
 
