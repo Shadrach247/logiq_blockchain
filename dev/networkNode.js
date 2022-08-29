2d9844c1dbbc20abcd7af0efd9006864e7382867
Shadrach247 <soshadrach@gmail.com>
2022-08-29 14:10:30 +0100

    update dev & package

--- dev/networkNode.js
+++ dev/networkNode.js
@@ -1,13 +1,46 @@
 const express = require('express');
 const app = express();
+const { v1: uuid } = require('uuid');
+const bodyParser = require('body-parser');
+const Blockchain = require('./blockchain');
+const port = process.argv[2];
 
+const bitcoin = new Blockchain();
+const nodeAddress = uuid().split('-').join('');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get('/blockchain', function(req, res) {});
+app.get('/blockchain', function(req, res) {
+    res.send(bitcoin);
+});
 
-app.post('/transaction', function(req, res) {});
+app.post('/transaction', function(req, res) {
+    const blockIndex = bitcoin.createNewTransaction(req.body.amount, req.body.sender,
+        req.body.recipient);
+        res.json({note: `Transaction will be added on block ${blockIndex}.`}); 
+});
 
-app.get('/mine', function(req, res) {});
+app.get('/mine', function(req, res) {
+    const lastBlock = bitcoin.getLastBlock();
+    const previousBlockHash = lastBlock.hash;
+    const currentBlockData = {
+        transactions: bitcoin.pendingTransactions,
+        index: lastBlock.index + 1
+    };
 
-app.listen(3000, function() {
-    console.log('listening on port 3000...');
+    const nonce = bitcoin.proofOfWork(previousBlockHash, currentBlockData);
+    const blockHash = bitcoin.hashBlock(previousBlockHash, currentBlockData, nonce);
+    const newBlock = bitcoin.createNewBlock(nonce, previousBlockHash, blockHash);
+
+    bitcoin.createNewTransaction(12.5, "00", nodeAddress);
+    
+    res.json({
+        note: "New block mined successfully",
+        block: newBlock
+    });
+
+});
+
+app.listen(port, function() {
+    console.log(`listening on port ${port}...`);
 });
--- dev/test.js
+++ dev/test.js
@@ -2,43 +2,5 @@ const Blockchain = require('./blockchain');
 
 const bitcoin = new Blockchain();
 
-//bitcoin.createNewBlock(3427, 'AVBN3YA2DH23L97', '7aqabn789kmba');
-//bitcoin.createNewBlock(2222, 'ADW23YRU76JHD3B', '7aq21juon468p');
-//bitcoin.createNewBlock(1111, 'KJUY43WERXCK90K', '81ndhetro935i');
-
-//bitcoin.createNewBlock(342759, 'AVBN3YA2DH23L97', 'AF6YETQWDNHG84');
-
-//bitcoin.createNewTransaction(100, 'ALEXVBN3YA2DH23L97', 'JENNM5TAQD567OKJL1');
-//bitcoin.createNewBlock(569237, 'A5QW29IJKU7E43K', 'IUY19R54JNMCVX');
-
-//bitcoin.createNewTransaction(100, 'ALEXVBN3YA2DH23L97', 'JENNM5TAQD567OKJL1');
-//bitcoin.createNewTransaction(50, 'ALEXVBN3YA2DH23L97', 'JENNM5TAQD567OKJL1');
-//bitcoin.createNewTransaction(400, 'ALEXVBN3YA2DH23L97', 'JENNM5TAQD567OKJL1');
-//bitcoin.createNewBlock(987123, 'KJ3F289UYCVXNJU', 'DG3YWTSR36UNLK1');
-
-/*const previousBlockHash = '23RGH45KMNLOPIZXACV345G7H89N';
-
-const currentBlockData = [
-    {
-        amount: 10,
-        sender: 'A56DGHI899GVBKLS',
-        recipient: '34GADFTICBZNXML',
-    },
-    {
-        amount: 65,
-        sender: 'A56DGHI899GVBKLS',
-        recipient: '34GADFTICBZNXML',
-    },
-    {
-        amount: 40,
-        sender: 'A56DGHI899GVBKLS',
-        recipient: '34GADFTICBZNXML',
-    }
-];
-
-const nonce = 156120;
-
-//console.log(bitcoin.proofOfWork(previousBlockHash, currentBlockData));
-console.log(bitcoin.hashBlock(previousBlockHash, currentBlockData, nonce)); */
 
 console.log(bitcoin);
