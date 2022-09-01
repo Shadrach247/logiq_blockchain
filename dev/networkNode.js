fb11106491762e5c083d1b1e936e73c8a9a780cc
Shadrach247 <soshadrach@gmail.com>
2022-09-01 14:53:58 +0100

    update dev & packages

--- dev/networkNode.js
+++ dev/networkNode.js
@@ -5,11 +5,48 @@ const bodyParser = require('body-parser');
 const Blockchain = require('./blockchain');
 const port = process.argv[2];
 const rp = require('request-promise');
+const { json } = require('body-parser');
 
 const bitcoin = new Blockchain();
 const nodeAddress = uuid().split('-').join('');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+
+
+app.post('/register-nodes-bulk', function(req, res) {
+
+});
+
+app.get('/blockchain', function(req, res) {
+    res.send(bitcoin);
+});
+
+app.post('/transaction', function(req, res) {
+    const blockIndex = bitcoin.createNewTransaction(req.body.amount, req.body.sender,
+        req.body.recipient);
+        res.json({note: `Transaction will be added on block ${blockIndex}.`}); 
+});
+
+app.get('/mine', function(req, res) {
+    const lastBlock = bitcoin.getLastBlock();
+    const previousBlockHash = lastBlock.hash;
+    const currentBlockData = {
+        transactions: bitcoin.pendingTransactions,
+        index: lastBlock.index + 1
+    };
+
+    const nonce = bitcoin.proofOfWork(previousBlockHash, currentBlockData);
+    const blockHash = bitcoin.hashBlock(previousBlockHash, currentBlockData, nonce);
+    const newBlock = bitcoin.createNewBlock(nonce, previousBlockHash, blockHash);
+
+    bitcoin.createNewTransaction(12.5, "00", nodeAddress);
+    
+    res.json({
+        note: "New block mined successfully",
+        block: newBlock
+    });
+
+});
 
 app.post('/register-and-broadcast-node', function(req, res) {
     const newNodeUrl = req.body.newNodeUrl;
@@ -45,40 +82,15 @@ app.post('/register-and-broadcast-node', function(req, res) {
     
 });
 
-app.post('/register-node', function () {});
-app.post('/register-nodes-bulk', function(req, res) {
-
-});
-
-app.get('/blockchain', function(req, res) {
-    res.send(bitcoin);
-});
-
-app.post('/transaction', function(req, res) {
-    const blockIndex = bitcoin.createNewTransaction(req.body.amount, req.body.sender,
-        req.body.recipient);
-        res.json({note: `Transaction will be added on block ${blockIndex}.`}); 
-});
-
-app.get('/mine', function(req, res) {
-    const lastBlock = bitcoin.getLastBlock();
-    const previousBlockHash = lastBlock.hash;
-    const currentBlockData = {
-        transactions: bitcoin.pendingTransactions,
-        index: lastBlock.index + 1
-    };
-
-    const nonce = bitcoin.proofOfWork(previousBlockHash, currentBlockData);
-    const blockHash = bitcoin.hashBlock(previousBlockHash, currentBlockData, nonce);
-    const newBlock = bitcoin.createNewBlock(nonce, previousBlockHash, blockHash);
-
-    bitcoin.createNewTransaction(12.5, "00", nodeAddress);
-    
-    res.json({
-        note: "New block mined successfully",
-        block: newBlock
-    });
-
+// register a node with the network
+app.post('/register-node', function (req, res) {
+    const newNodeUrl = req.body.newNodeUrl;
+    const notCurrentNode = bitcoin.currentNodeUrl !== newNodeUrl;
+          const nodeNotAlreadyPresent = 
+                bitcoin.networkNodes.indexOf(newNodeUrl) == -1;
+            if (nodeNotAlreadyPresent && notCurrentNode) bitcoin.networkNodes.push(newNodeUrl);
+          bitcoin.networkNodes.push(newNodeUrl);
+          res.json({ note: 'New node registered successfully.' });
 });
 
 app.listen(port, function() {
